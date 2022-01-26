96e6b7a1977e7642d6614634e7554f5bf63530ae
Charlie Birks <charlie@daft.games>
2022-01-26 23:12:37 +0000

    Pallete swap custom wall/floor colours

--- level.cpp
+++ level.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+
 #include "engine/api.hpp"
 #include "engine/engine.hpp"
 
@@ -15,6 +17,9 @@ struct MapTile {
     uint8_t floor;
     uint8_t walls[4]; // bottom, right, top, left
     uint8_t object = 0;
+
+    blit::Pen floor_colour = {};
+    blit::Pen wall_colour[4] = {};
 };
 
 enum WallSide {
@@ -73,17 +78,111 @@ static const uint8_t wall_to_hidden[]{
     5, // wall + door
 };
 
+// TODO: this is getting silly, need something to edit this with
+static const blit::Pen default_col{}, yellow_col{255, 255, 128}, cyan_col{200, 255, 255}, green_col{50, 100, 50},
+                       off_white_col{240, 240, 240}, grey60_col{60, 60, 60}, grey100_col{100, 100, 100};
+
 static const int map_width = 10, map_height = 9;
 static const MapTile map[map_width * map_height]{
-    {1, {0, 0, 1, 1}, 24}, {1, {0, 0, 1, 0}}, {1, {0, 0, 2, 0}, 24}, {1, {0, 1, 1, 0}}, {1, {0, 0, 1, 0}}, {1, {0, 0, 1, 0}}, {1, {0, 0, 1, 0}, 24}, {1, {0, 0, 2, 0}, 24}, {1, {0, 0, 1, 0}, 24}, {1, {0, 1, 1, 0}},
-    {1, {0, 0, 0, 1}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}},
-    {1, {0, 0, 0, 1}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 3, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 2, 0, 0}},
-    {1, {0, 0, 0, 1}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}, 24}, {1, {0, 1, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}},
-    {1, {0, 0, 1, 1}}, {1, {0, 0, 3, 0}}, {1, {0, 0, 1, 0}}, {1, {0, 1, 1, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 3, 0, 0}},
-    {1, {0, 0, 0, 1}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}},
-    {1, {0, 0, 0, 1}}, {1, {0, 0, 0, 0}, 24}, {1, {0, 0, 0, 0}}, {1, {0, 3, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 2, 0, 0}},
-    {1, {0, 0, 0, 2}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 0, 0, 0}}, {1, {0, 1, 0, 0}},
-    {1, {1, 0, 0, 1}}, {1, {1, 0, 0, 0}}, {1, {2, 0, 0, 0}}, {1, {1, 1, 0, 0}}, {1, {1, 0, 0, 0}}, {1, {1, 0, 0, 0}}, {1, {1, 0, 0, 0}}, {1, {2, 0, 0, 0}, 24}, {1, {1, 0, 0, 0}}, {1, {1, 1, 0, 0}},
+    // Y = 0
+    {1, {0, 0, 1, 1}, 24,  grey60_col, {default_col, default_col, off_white_col, off_white_col}},
+    {1, {0, 0, 1, 0},  0, grey100_col, {default_col, default_col, off_white_col,   default_col}},
+    {1, {0, 0, 2, 0}, 24,  grey60_col, {default_col, default_col, off_white_col,   default_col}},
+    {1, {0, 1, 1, 0},  0, grey100_col, {default_col,  yellow_col, off_white_col,   default_col}},
+    {1, {0, 0, 1, 0},  0, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    {1, {0, 0, 1, 0},  0, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    {1, {0, 0, 1, 0}, 24, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    {1, {0, 0, 2, 0}, 24, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    {1, {0, 0, 1, 0}, 24, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    {1, {0, 1, 1, 0},  0, default_col, {default_col, default_col,    yellow_col,   default_col}},
+    // Y = 1
+    {1, {0, 0, 0, 1},  0, grey100_col, {default_col, default_col,   default_col, off_white_col}},
+    {1, {0, 0, 0, 0},  0,  grey60_col},
+    {1, {0, 0, 0, 0},  0, grey100_col},
+    {1, {0, 1, 0, 0},  0,  grey60_col, {default_col,  yellow_col,   default_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 1, 0, 0},  0},
+    // Y = 2
+    {1, {0, 0, 0, 1},  0,  grey60_col, {default_col, default_col,   default_col, off_white_col}},
+    {1, {0, 0, 0, 0},  0, grey100_col},
+    {1, {0, 0, 0, 0},  0,  grey60_col},
+    {1, {0, 3, 0, 0},  0, grey100_col, {default_col,  yellow_col,   default_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 2, 0, 0},  0},
+    // Y = 3
+    {1, {0, 0, 0, 1},  0, grey100_col, {default_col, default_col,   default_col, off_white_col}},
+    {1, {0, 0, 0, 0},  0,  grey60_col},
+    {1, {0, 0, 0, 0}, 24, grey100_col},
+    {1, {0, 1, 0, 0},  0,  grey60_col, {default_col,  yellow_col,   default_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 1, 0, 0},  0},
+    // Y = 4
+    {1, {0, 0, 1, 1},  0,   green_col, {default_col, default_col,      cyan_col,      cyan_col}},
+    {1, {0, 0, 3, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 0, 1, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 1, 1, 0},  0,   green_col, {default_col,  yellow_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 3, 0, 0},  0},
+    // Y = 5
+    {1, {0, 0, 0, 1},  0,   green_col, {default_col, default_col,      cyan_col,      cyan_col}},
+    {1, {0, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 1, 0, 0},  0,   green_col, {default_col,  yellow_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 1, 0, 0},  0},
+    // Y = 6
+    {1, {0, 0, 0, 1},  0,   green_col, {default_col, default_col,      cyan_col,      cyan_col}},
+    {1, {0, 0, 0, 0}, 24,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 3, 0, 0},  0,   green_col, {default_col,  yellow_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 2, 0, 0},  0},
+    // Y = 7
+    {1, {0, 0, 0, 2},  0,   green_col, {default_col, default_col,      cyan_col,      cyan_col}},
+    {1, {0, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {0, 1, 0, 0},  0,   green_col, {default_col,  yellow_col,      cyan_col,   default_col}},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 0, 0, 0},  0},
+    {1, {0, 1, 0, 0},  0},
+    // Y = 8
+    {1, {1, 0, 0, 1},  0,   green_col, {default_col, default_col,      cyan_col,      cyan_col}},
+    {1, {1, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {2, 0, 0, 0},  0,   green_col, {default_col, default_col,      cyan_col,   default_col}},
+    {1, {1, 1, 0, 0},  0,   green_col, {default_col,  yellow_col,      cyan_col,   default_col}},
+    {1, {1, 0, 0, 0},  0},
+    {1, {1, 0, 0, 0},  0},
+    {1, {1, 0, 0, 0},  0},
+    {1, {2, 0, 0, 0}, 24},
+    {1, {1, 0, 0, 0},  0},
+    {1, {1, 1, 0, 0},  0},
 };
 
 static bool walls_hidden = false;
@@ -112,17 +211,39 @@ void Level::render() {
 
     screen.sprites = tiles;
 
+    // colours from the floor/walls may be reused, so save the original values
+    blit::Pen orig_cols[9];
+    memcpy(orig_cols, tiles->palette, sizeof(orig_cols));
+
     auto draw_sprite = [](int x, int y, const SpriteInfo &sprite){
         screen.sprite({sprite.sheet_x, sprite.sheet_y, sprite.sheet_w, sprite.sheet_h}, {x - sprite.center_x, y - sprite.center_y});
     };
 
-    auto draw_wall = [&draw_sprite](int x, int y, const MapTile &tile, WallSide side) {
+    auto draw_wall = [&draw_sprite, &orig_cols](int x, int y, const MapTile &tile, WallSide side) {
         if(tile.walls[side]) {
             int base_sprite = tile.walls[side];
             if(walls_hidden)
                 base_sprite = wall_to_hidden[base_sprite];
 
+            // colour multipliers for shading (8-bit fraction, -1 so a scale of 1.0 will fit)
+            const uint8_t col_mul_r[]{255, 162, 144,  92};
+            const uint8_t col_mul_g[]{255, 162, 164,  99};
+            const uint8_t col_mul_b[]{255, 159, 172, 108};
+
+            blit::Pen wall_col{99, 102, 99};
+
+            if(tile.wall_colour[side].a)
+                wall_col = tile.wall_colour[side];
+
+            screen.sprites->palette[5 + side] = {
+                wall_col.r * (col_mul_r[side] + 1) / 256,
+                wall_col.g * (col_mul_g[side] + 1) / 256,
+                wall_col.b * (col_mul_b[side] + 1) / 256
+            };
+
             draw_sprite(x, y, sprites[base_sprite * 4 + side]);
+
+            screen.sprites->palette[5 + side] = orig_cols[5 + side];
         }
     };
 
@@ -140,8 +261,21 @@ void Level::render() {
             draw_wall(center_x, center_y, tile, Side_Left);
 
             // floor
-            if(tile.floor)
+            if(tile.floor) {
+                blit::Pen floor_col{154, 128, 103};
+                if(tile.floor_colour.a)
+                    floor_col = tile.floor_colour;
+
+                tiles->palette[1] = floor_col;
+                tiles->palette[2] = {floor_col.r * 234 / 256, floor_col.g * 234 / 256, floor_col.b * 234 / 256}; // grid lines
+                tiles->palette[3] = {floor_col.r * 156 / 256, floor_col.g * 156 / 256, floor_col.b * 156 / 256}; // shading
+                tiles->palette[4] = {floor_col.r * 103 / 256, floor_col.g * 103 / 256, floor_col.b * 103 / 256}; // more shading
+
                 draw_sprite(center_x, center_y, sprites[tile.floor - 1]);
+
+                for(int i = 1; i < 5; i++)
+                    tiles->palette[i] = orig_cols[i];
+            }
 
             if(tile.object)
                 draw_sprite(center_x, center_y, sprites[tile.object]);
