01f34631e990cbedf9062d06cac4f58a005502bd
Charlie Birks <charlie@daft.games>
2023-03-29 19:12:29 +0100

    Adjust bed sleep effectivness

--- entity-info.cpp
+++ entity-info.cpp
@@ -4,7 +4,7 @@ const EntityInfo entity_bathroom_shower{1, 1, 44, {}};
 const EntityInfo entity_bathroom_sink{1, 1, 48, {}};
 const EntityInfo entity_bathroom_toilet{1, 1, 52, {}};
 
-const EntityInfo entity_bedroom_bed{1, 2, 24, {0.00002f}};
+const EntityInfo entity_bedroom_bed{1, 2, 24, {0.000025f}};
 
 const EntityInfo entity_kitchen_fridge{1, 1, 40, {}};
 const EntityInfo entity_kitchen_oven{1, 1, 36, {}};
