797ef1332ea71d61f141372fc54c5ee84f3363d7
Charlie Birks <charlie@daft.games>
2023-03-29 19:13:56 +0100

    Add some sleeping behaviour

--- person.cpp
+++ person.cpp
@@ -29,6 +29,46 @@ public:
     };
 };
 
+class FindEntityPositionForNeed final : public behaviour_tree::Node {
+public:
+    constexpr FindEntityPositionForNeed(Person::Need need) : need(need){} 
+
+    void init(BehaviourTreeState &state) const override {}
+
+    void deinit(BehaviourTreeState &state) const override {}
+
+    behaviour_tree::Status update(BehaviourTreeState &state) const override {
+        using behaviour_tree::Status;
+        auto world = std::any_cast<World *>(state.get_variable(PersonVar_WorldPtr));
+
+        auto ent_ids = world->get_entities_for_need(need);
+
+        if(ent_ids.empty())
+            return Status::Failed;
+
+        // TODO: sort by effect/distance?
+        // TODO: split returning the entities and getting the position
+
+        auto &ent = world->get_entity(ent_ids[0]);
+        auto target_pos = ent.get_tile_position();
+
+        // adjust to the tile in front
+        switch(ent.get_rotation()) {
+            case 0: target_pos.y++; break;
+            case 1: target_pos.x++; break;
+            case 2: target_pos.y--; break;
+            case 3: target_pos.x--; break;
+        }
+
+        state.set_variable(PersonVar_Position, target_pos);
+
+        return Status::Success;
+    };
+
+private:
+    Person::Need need;
+};
+
 class MoveToNode final : public behaviour_tree::Node {
 public:
     struct State {
@@ -129,15 +169,121 @@ public:
     };
 };
 
+// are we tired?
+class CheckTiredNode final : public behaviour_tree::Node {
+public:
+    void init(BehaviourTreeState &state) const override {}
+
+    void deinit(BehaviourTreeState &state) const override {}
+
+    behaviour_tree::Status update(BehaviourTreeState &state) const override {
+        using behaviour_tree::Status;
+
+        auto entity_index = std::any_cast<uint16_t>(state.get_variable(PersonVar_EntIndex));
+        auto world = std::any_cast<World *>(state.get_variable(PersonVar_WorldPtr));
+
+        // get person
+        auto person = world->find_person(entity_index);
+
+        if(!person)
+            return Status::Failed;
+
+        // TODO: also check if it's late?
+
+        return person->get_need(Person::Need::Sleep) < 0.2f ? Status::Success : Status::Failed;
+    };
+};
+
+class SleepNode final : public behaviour_tree::Node {
+public:
+    struct State {
+        blit::Point orig_pos;
+        bool started_sleep = false;
+    };
+
+    void init(BehaviourTreeState &state) const override {
+        auto entity_index = std::any_cast<uint16_t>(state.get_variable(PersonVar_EntIndex));
+        auto world = std::any_cast<World *>(state.get_variable(PersonVar_WorldPtr));
+
+        State node_state;
+        node_state.orig_pos = world->get_entity(entity_index).get_tile_position();
+
+        state.create_node_state(this, node_state);
+    }
+
+    void deinit(BehaviourTreeState &state) const override {
+        state.destroy_node_state(this);
+    }
+
+    behaviour_tree::Status update(BehaviourTreeState &state) const override {
+        using behaviour_tree::Status;
+
+        auto &node_state = std::any_cast<State &>(state.get_node_state(this));
+
+        auto entity_index = std::any_cast<uint16_t>(state.get_variable(PersonVar_EntIndex));
+        auto world = std::any_cast<World *>(state.get_variable(PersonVar_WorldPtr));
+
+        // get person
+        auto person = world->find_person(entity_index);
+
+        if(!person)
+            return Status::Failed;
+
+        if(person->is_moving())
+            return Status::Running;
+
+        // should be next to or on a bed
+        auto &ent = world->get_entity(entity_index);
+        bool in_bed = world->has_entity_for_need(ent.get_tile_position(), Person::Need::Sleep);
+
+        if(!in_bed && !node_state.started_sleep) {
+            // find the bed
+            static const blit::Point offsets[]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+            for(auto &off : offsets) {
+                if(world->has_entity_for_need(ent.get_tile_position() + off, Person::Need::Sleep)) {
+                    // get in
+                    person->move_to_tile(ent.get_tile_position() + off);
+                    break;
+                }
+            }
+
+            // couldn't find it, fail
+            if(!person->is_moving())
+                return Status::Failed;
+
+            node_state.started_sleep = true;
+           
+            printf("in bed %u\n", world->get_time());
+            return Status::Running;
+        } else if(in_bed && person->get_need(Person::Need::Sleep) == 1.0f) {
+            // get out
+            person->move_to_tile(node_state.orig_pos);
+            printf("out bed %u\n", world->get_time());
+            return Status::Running;
+        }
+
+        return in_bed ? Status::Running : Status::Success;
+    };
+};
+
 static const RandomPositionNode random_pos;
 static const MoveToNode move_to;
 static const ThinkNode think;
 
 static const behaviour_tree::SequenceNode<3> move_sequence({&random_pos, &move_to, &think});
 
-static const behaviour_tree::RepeaterNode tree_root(&move_sequence);
+static const CheckTiredNode check_tired;
+static const FindEntityPositionForNeed find_bed(Person::Need::Sleep);
+static const SleepNode sleep;
 
-Person::Person(World &world, uint16_t entity_index) : world(world), entity_index(entity_index), behaviour_tree(&tree_root) {
+static const behaviour_tree::SequenceNode<4> sleep_sequence({&check_tired, &find_bed, &move_to, &sleep});
+
+static const behaviour_tree::SelectorNode<2> idle_selector({&sleep_sequence, &move_sequence});
+
+static const behaviour_tree::RepeaterNode tree_root(&idle_selector);
+
+Person::Person(World &world, uint16_t entity_index) : world(world), entity_index(entity_index), behaviour_tree(&tree_root), needs{0.2f} {
     behaviour_tree.set_variable(PersonVar_EntIndex, entity_index);
     behaviour_tree.set_variable(PersonVar_WorldPtr, &world);
 }
